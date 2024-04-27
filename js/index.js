cef89cf47ebb8c3c0d6a6066ed2e2c829d0ad12a
anik558363 <anikmondol558363@gmil.com>
2024-04-27 15:51:30 +0600

    data load and search function added in this pojects

--- js/index.js
+++ js/index.js
@@ -1,9 +1,80 @@
-const loadData = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
-    console.log(data);
+    const phones = data.data;
+
+    
+    displayPhones(phones);
 }
 
 
 
-loadData();
+const displayPhones = (phones) =>{
+
+
+    // 1. find the parent element
+    const cardContainer = document.getElementById('card_container');
+
+    // clear phone container cards before adding new cards 
+    cardContainer.textContent = '';
+
+    if(phones.length === 0){
+        alert('i have not found')
+    }
+
+
+    // display only first 12 data
+    phones = phones.slice(0,12);
+    console.log(phones);
+
+
+
+
+
+
+    phones.forEach((phone) => {
+        // 2. create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = 'card  bg-gray-200 shadow-xl';
+
+
+        //3. set inner html
+        phoneCard.innerHTML = `
+        
+        <figure class="px-10 pt-10">
+        <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+        </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>There are many variations of passages of available, but the majority have suffered</p>
+            <h4 class="text-2xl font-semibold">$999</h4>
+            <div class="card-actions">
+                <button class="btn btn-primary">Show Details</button>
+             </div>
+        </div>
+        `;
+
+        // 4. append chile elements
+        cardContainer.appendChild(phoneCard)
+    }); 
+}
+
+
+
+
+
+// handle search button
+
+const handleSearch = () => {
+    const searchFiled = document.getElementById('search_filed');
+    const searchText = searchFiled.value;
+    if(searchText === ''){
+        alert('give me valid text')
+    }else{
+        loadPhone(searchText);
+    }
+}
+
+
+
+// loadPhone();
